0162d8024efe4259e12e270e5cb5980796ec4955
danielk <kucharski22daniel@gmail.com>
2018-02-18 12:56:26 +0100

    initial commit

---
+++ Os_thread1/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Os_thread1
+//
+//  Created by daniel on 18.02.2018.
+//  Copyright © 2018 code masterss. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
