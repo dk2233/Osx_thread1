f6cd44612848ca353a436066fe2b9a68a85f3881
danielk <kucharski22daniel@gmail.com>
2018-02-25 16:44:45 +0100

    Added dipatch functionality to have "wait" functionality but it doesnt work

--- Os_thread1/main.m
+++ Os_thread1/main.m
@@ -14,13 +14,22 @@ int main(int argc, const char * argv[]) {
     
     
     MyClass_thread  *woteczki = [[MyClass_thread alloc] init];
+    double delayInSeconds = 5.0;
     
-        
+    int nr_int = 0U;
     @autoreleasepool {
         // insert code here...
         NSLog(@"Hello, World of threads!");
         
         while(1){
+            //NSLog(@"aaa %d",nr_int);
+            dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC));
+            dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
+                //NSLog(@"aaa %d",nr_int);
+                NSLog(@"aaa");
+            });
+            //printf(" seconds %d",nr_int);
+            nr_int++;
              
         }
         
