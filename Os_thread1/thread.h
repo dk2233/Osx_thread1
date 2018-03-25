d2e07537c2fbf2bf317cb9a48f8943d7b47f3374
danielk <kucharski22daniel@gmail.com>
2018-03-25 11:56:50 +0200

    added #if for #if TARGET_OS_OSX && !defined(TARGET_OS_IPHONE) to hide NSTASK usage

--- Os_thread1/thread.h
+++ Os_thread1/thread.h
@@ -8,19 +8,21 @@
 
 #ifndef thread_h
 #define thread_h
-
+#include <TargetConditionals.h>
 
 #define DELAY_IN_SECONDS   1
 #define HOW_LONG_FIRST      5
 #define HOW_LONG_SECOND      10
-
+//#define TARGET_OS_OSX (!(TARGET_OS_IOS || TARGET_OS_TV || TARGET_OS_WATCH))
 
 @interface MyClass_thread : NSObject
 
 @property  int seconds;
 @property  dispatch_time_t DelayTime;
 @property  dispatch_queue_t MyQueue;
+#if TARGET_OS_OSX && !defined(TARGET_OS_IPHONE)
 @property  NSTask *newtask;
+#endif
 @property  dispatch_group_t MyGroup;
 
 
